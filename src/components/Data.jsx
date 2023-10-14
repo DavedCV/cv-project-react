9b53630160af12fbab47da944cf7ab169187f184
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-10-13 19:26:57 -0500

    Add more styles to the data section forms

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { useState } from "react";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PERSONAL_DATA = ["full name", "email", "phone number", "location"];
 const EDUCATION = ["school name", "degree", "start date", "end date"];
@@ -41,29 +43,30 @@ export default function Data() {
 function Section({ className, data, isActive, toggle }) {
   return isActive ? (
     <div
-      className={className}
+      className={`${className} active`}
       onClick={toggle()}
       onKeyUp={toggle()}
       tabIndex={0}
     >
-      <h2>{className}</h2>
+      <div className="section-header">
+        <h2>{className}</h2>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </div>
       <form>
         {data.map((info) => (
           <div key={info} className={`${info}-entry`}>
-            <label htmlFor={info}>{info.toUpperCase()}</label>
+            <label htmlFor={info}>{info}</label>
             <input type="text" id={info} />
           </div>
         ))}
       </form>
     </div>
   ) : (
-    <div
-      className={className}
-      onClick={toggle}
-      onKeyUp={toggle}
-      tabIndex={0}
-    >
-      <h2>{className}</h2>
+    <div className={className} onClick={toggle} onKeyUp={toggle} tabIndex={0}>
+      <div className="section-header">
+        <h2>{className}</h2>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </div>
     </div>
   );
 }
