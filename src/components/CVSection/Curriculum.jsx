5fcbed2da5389531a92cb0f6b8dddb82fbf76dd7
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-10-17 17:43:39 -0500

    Add the experience section to the cv layout

--- src/components/CVSection/Curriculum.jsx
+++ src/components/CVSection/Curriculum.jsx
@@ -7,23 +7,58 @@ export default function Curriculum({ sectionsData }) {
 
   return (
     <div className="curriculum">
-      <div className="personal-info">
-        <h1 className="fullname-cv">{personalData["full name"]}</h1>
-        <div>
-          {Object.keys(personalData)
-            .filter((data) => data != "full name" && data != "personal statement")
-            .map((data) => (
-              <p
-                key={`${data.replace(" ", "")}-cv`}
-                className={`${data.replace(" ", "")}-cv`}
-              >
-                <span>{data}: </span>
-                {personalData[data]}
-              </p>
-            ))}
+      <PersonalInfo personalData={personalData} />
+      <h2 className="section-header">Profesional Experience</h2>
+      <Experience experience={experience} />
+    </div>
+  );
+}
+
+function Experience({ experience }) {
+  return (
+    <div className="experience-info">
+      {experience.map((job) => (
+        <div
+          className="experience-cv"
+          key={`${job["position"]}-${job["company name"]}-cv`}
+        >
+          <div className="job-date-cv">
+            <p>
+              {job["start date"]} - {job["end date"]}
+            </p>
+          </div>
+          <div className="job-details-cv">
+            <h3>{job["position"]}</h3>
+            <h4>{job["company name"]}</h4>
+            <p>{job["job description"]}</p>
+          </div>
         </div>
-        <p className="personalstatement-cv">{personalData["personal statement"]}</p>
+      ))}
+    </div>
+  );
+}
+
+function PersonalInfo({ personalData }) {
+  return (
+    <div className="personal-info">
+      <h1 className="fullname-cv">{personalData["full name"]}</h1>
+      <h2 className="position-cv">{personalData["position"]}</h2>
+      <div>
+        {Object.keys(personalData)
+          .filter((data) => data != "full name" && data != "personal statement")
+          .map((data) => (
+            <p
+              key={`${data.replace(" ", "")}-cv`}
+              className={`${data.replace(" ", "")}-cv`}
+            >
+              <span>{data}: </span>
+              {personalData[data]}
+            </p>
+          ))}
       </div>
+      <p className="personalstatement-cv">
+        {personalData["personal statement"]}
+      </p>
     </div>
   );
 }
--- src/components/DetailsSection/formEntriesData.js
+++ src/components/DetailsSection/formEntriesData.js
@@ -1,7 +1,21 @@
 const formsEntries = {
-  personal: ["full name", "email", "phone number", "location", "linkedin", "personal statement"],
+  personal: [
+    "full name",
+    "email",
+    "position",
+    "phone number",
+    "location",
+    "linkedin",
+    "personal statement",
+  ],
   education: ["school name", "degree", "start date", "end date"],
-  experience: ["company name", "start date", "end date", "job description"],
+  experience: [
+    "position",
+    "company name",
+    "start date",
+    "end date",
+    "job description",
+  ],
 };
 
 export default formsEntries;
